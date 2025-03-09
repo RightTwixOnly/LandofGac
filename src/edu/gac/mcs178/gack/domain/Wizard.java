3cd8d78b813c72a48e53284411a89d7425a47190
Simon Leonard <simonbleo@gmail.com>
2025-03-09 18:27:57 -0500

    wkzz

--- src/edu/gac/mcs178/gack/domain/Wizard.java
+++ src/edu/gac/mcs178/gack/domain/Wizard.java
@@ -2,6 +2,8 @@ package edu.gac.mcs178.gack.domain;
 
 import java.util.List;
 
+import edu.gac.mcs178.gack.Utility;
+
 public class Wizard extends AutoPerson {
 	
 	private Place chamber;
@@ -14,10 +16,11 @@ public class Wizard extends AutoPerson {
 	@Override
 	public void act() {
 		List<Scroll> scrolls = Scroll.scrollsIn(getPlace());
+		int scrollOrder = Utility.randInt(scrolls.size());
 		if ((!scrolls.isEmpty()) && (!getPlace().equals(chamber))) {
-			take(scrolls.get(0));
+			take(scrolls.get(scrollOrder));
 			moveTo(chamber);
-			lose(scrolls.get(0));
+			lose(scrolls.get(scrollOrder));
 		} else {
 			super.act();
 		}
